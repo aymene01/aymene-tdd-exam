f80b009556552eecb39e8d1d6df641ebc888cc87
aymene01 <70906904+aymene01@users.noreply.github.com>
2025-03-04 17:21:34 +0100

    feat(hand): Implement hand comparison logic in compareTo method (#4)

--- src/Hand/Hand.ts
+++ src/Hand/Hand.ts
@@ -137,11 +137,168 @@ export class Hand {
   }
 
   compareTo(other: Hand): number {
-    const thisHandType = this.getHandType();
-    const otherHandType = other.getHandType();
+    const thisType = this.getHandType();
+    const otherType = other.getHandType();
 
-    if (thisHandType !== otherHandType) {
-      return HAND_TYPE_RANKS[thisHandType] - HAND_TYPE_RANKS[otherHandType];
+    const typeComparison = HAND_TYPE_RANKS[thisType] - HAND_TYPE_RANKS[otherType];
+    if (typeComparison !== 0) {
+      return typeComparison;
+    }
+
+    switch (thisType) {
+      case HandType.ROYAL_FLUSH:
+        return 0;
+
+      case HandType.STRAIGHT_FLUSH:
+      case HandType.STRAIGHT:
+        return this.compareHighCard(other);
+
+      case HandType.FOUR_OF_A_KIND:
+        return this.compareFourOfAKind(other);
+
+      case HandType.FULL_HOUSE:
+        return this.compareFullHouse(other);
+
+      case HandType.FLUSH:
+      case HandType.HIGH_CARD:
+        return this.compareHighCard(other);
+
+      case HandType.THREE_OF_A_KIND:
+        return this.compareThreeOfAKind(other);
+
+      case HandType.TWO_PAIR:
+        return this.compareTwoPair(other);
+
+      case HandType.ONE_PAIR:
+        return this.compareOnePair(other);
+
+      default:
+        return 0;
+    }
+  }
+
+  private compareHighCard(other: Hand): number {
+    for (let i = 0; i < this.cards.length; i++) {
+      const comparison = this.cards[i].rank - other.cards[i].rank;
+      if (comparison !== 0) {
+        return comparison;
+      }
+    }
+    return 0;
+  }
+
+  private getFourOfAKindRank(): number {
+    const rankCounts = this.countRanks();
+    for (const rank in rankCounts) {
+      if (rankCounts[rank] === 4) {
+        return parseInt(rank);
+      }
+    }
+    return 0;
+  }
+
+  private compareFourOfAKind(other: Hand): number {
+    const thisRank = this.getFourOfAKindRank();
+    const otherRank = other.getFourOfAKindRank();
+    return thisRank - otherRank;
+  }
+
+  private getThreeOfAKindRank(): number {
+    const rankCounts = this.countRanks();
+    for (const rank in rankCounts) {
+      if (rankCounts[rank] === 3) {
+        return parseInt(rank);
+      }
+    }
+    return 0;
+  }
+
+  private getPairRanks(): number[] {
+    const rankCounts = this.countRanks();
+    const pairRanks: number[] = [];
+
+    for (const rank in rankCounts) {
+      if (rankCounts[rank] === 2) {
+        pairRanks.push(parseInt(rank));
+      }
+    }
+
+    return pairRanks.sort((a, b) => b - a);
+  }
+
+  private compareFullHouse(other: Hand): number {
+    const thisThreeRank = this.getThreeOfAKindRank();
+    const otherThreeRank = other.getThreeOfAKindRank();
+
+    if (thisThreeRank !== otherThreeRank) {
+      return thisThreeRank - otherThreeRank;
+    }
+
+    const thisPairRank = this.getPairRanks()[0];
+    const otherPairRank = other.getPairRanks()[0];
+
+    return thisPairRank - otherPairRank;
+  }
+
+  private compareThreeOfAKind(other: Hand): number {
+    const thisThreeRank = this.getThreeOfAKindRank();
+    const otherThreeRank = other.getThreeOfAKindRank();
+
+    if (thisThreeRank !== otherThreeRank) {
+      return thisThreeRank - otherThreeRank;
+    }
+
+    const thisRemainingCards = this.cards.filter(card => card.rank !== thisThreeRank);
+    const otherRemainingCards = other.cards.filter(card => card.rank !== otherThreeRank);
+
+    for (let i = 0; i < thisRemainingCards.length; i++) {
+      const comparison = thisRemainingCards[i].rank - otherRemainingCards[i].rank;
+      if (comparison !== 0) {
+        return comparison;
+      }
+    }
+
+    return 0;
+  }
+
+  private compareTwoPair(other: Hand): number {
+    const thisPairRanks = this.getPairRanks();
+    const otherPairRanks = other.getPairRanks();
+
+    if (thisPairRanks[0] !== otherPairRanks[0]) {
+      return thisPairRanks[0] - otherPairRanks[0];
+    }
+
+    if (thisPairRanks[1] !== otherPairRanks[1]) {
+      return thisPairRanks[1] - otherPairRanks[1];
+    }
+
+    const thisRemainingCard = this.cards.find(card => !thisPairRanks.includes(card.rank));
+    const otherRemainingCard = other.cards.find(card => !otherPairRanks.includes(card.rank));
+
+    if (thisRemainingCard && otherRemainingCard) {
+      return thisRemainingCard.rank - otherRemainingCard.rank;
+    }
+
+    return 0;
+  }
+
+  private compareOnePair(other: Hand): number {
+    const thisPairRank = this.getPairRanks()[0];
+    const otherPairRank = other.getPairRanks()[0];
+
+    if (thisPairRank !== otherPairRank) {
+      return thisPairRank - otherPairRank;
+    }
+
+    const thisRemainingCards = this.cards.filter(card => card.rank !== thisPairRank);
+    const otherRemainingCards = other.cards.filter(card => card.rank !== otherPairRank);
+
+    for (let i = 0; i < thisRemainingCards.length; i++) {
+      const comparison = thisRemainingCards[i].rank - otherRemainingCards[i].rank;
+      if (comparison !== 0) {
+        return comparison;
+      }
     }
 
     return 0;
