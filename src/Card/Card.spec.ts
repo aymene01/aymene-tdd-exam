380281e366b0a3341c7682f816cebf9dc3e87e3a
Aymene BOUSBIA <aymene.bousbia@ledger.fr>
2025-03-04 14:56:59 +0100

    style(card): code format

--- src/Card/Card.spec.ts
+++ src/Card/Card.spec.ts
@@ -4,14 +4,14 @@ import { Card, Rank, Suit } from './Card';
 describe('Card', () => {
   it('should create a card with a valid rank and suit', () => {
     const card = new Card(Rank.ACE, Suit.HEARTS);
-    
+
     expect(card.rank).toBe(Rank.ACE);
     expect(card.suit).toBe(Suit.HEARTS);
   });
 
   it('should correctly format the card as a string', () => {
     const card = new Card(Rank.ACE, Suit.HEARTS);
-    
+
     expect(card.toString()).toBe('A♥');
   });
 
@@ -19,7 +19,7 @@ describe('Card', () => {
     const aceOfHearts = new Card(Rank.ACE, Suit.HEARTS);
     const kingOfHearts = new Card(Rank.KING, Suit.HEARTS);
     const aceOfSpades = new Card(Rank.ACE, Suit.SPADES);
-    
+
     expect(aceOfHearts.compareByRank(kingOfHearts)).toBeGreaterThan(0);
     expect(kingOfHearts.compareByRank(aceOfHearts)).toBeLessThan(0);
     expect(aceOfHearts.compareByRank(aceOfSpades)).toBe(0);
