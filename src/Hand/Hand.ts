1973a1887483047b28a8239dac01b067bc2035fe
Aymene BOUSBIA <aymene.bousbia@ledger.fr>
2025-03-04 15:30:45 +0100

    feat(hand): add test for getHandType method

--- src/Hand/Hand.ts
+++ src/Hand/Hand.ts
@@ -1,5 +1,31 @@
 import { Card, Rank } from '../Card';
 
+export enum HandType {
+  HIGH_CARD = 'High Card',
+  ONE_PAIR = 'One Pair',
+  TWO_PAIR = 'Two Pair',
+  THREE_OF_A_KIND = 'Three of a Kind',
+  STRAIGHT = 'Straight',
+  FLUSH = 'Flush',
+  FULL_HOUSE = 'Full House',
+  FOUR_OF_A_KIND = 'Four of a Kind',
+  STRAIGHT_FLUSH = 'Straight Flush',
+  ROYAL_FLUSH = 'Royal Flush',
+}
+
+const HAND_TYPE_RANKS: Record<HandType, number> = {
+  [HandType.HIGH_CARD]: 1,
+  [HandType.ONE_PAIR]: 2,
+  [HandType.TWO_PAIR]: 3,
+  [HandType.THREE_OF_A_KIND]: 4,
+  [HandType.STRAIGHT]: 5,
+  [HandType.FLUSH]: 6,
+  [HandType.FULL_HOUSE]: 7,
+  [HandType.FOUR_OF_A_KIND]: 8,
+  [HandType.STRAIGHT_FLUSH]: 9,
+  [HandType.ROYAL_FLUSH]: 10,
+};
+
 export class Hand {
   readonly cards: Card[];
 
@@ -17,17 +43,4 @@ export class Hand {
   getRank(): Rank {
     return this.cards[0].rank;
   }
-}
-
-export enum HandType {
-  HIGH_CARD = 'High Card',
-  ONE_PAIR = 'One Pair',
-  TWO_PAIR = 'Two Pair',
-  THREE_OF_A_KIND = 'Three of a Kind',
-  STRAIGHT = 'Straight',
-  FLUSH = 'Flush',
-  FULL_HOUSE = 'Full House',
-  FOUR_OF_A_KIND = 'Four of a Kind',
-  STRAIGHT_FLUSH = 'Straight Flush',
-  ROYAL_FLUSH = 'Royal Flush',
 }
